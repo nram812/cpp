e393eea1076cbc0d34292cd6deaef74ef6b0b710
Neelesh Rampal neelesh.rampal@niwa.co.nz <rampaln@w-nwp01.maui.niwa.co.nz>
2021-05-03 00:02:58 +0000

    Auto updated

--- automation.sh
+++ automation.sh
@@ -11,6 +11,8 @@ cd /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/niwa_soi
 
 /nesi/project/niwa00004/rampaln/bin/python /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/CEI/compute_cei.py
 
+/nesi/project/niwa00004/rampaln/bin/python /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/indices/code/compute_indices.py
+
 # Running the IOD scripts
 echo "checking for new data"
 /nesi/project/niwa00004/rampaln/bin/python /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/IOD/code/GetNOAAERSSTV5.py
@@ -22,7 +24,7 @@ echo "finished creating script"
 /nesi/project/niwa00004/rampaln/bin/python /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/IPO/code/Run_IPO.py
 echo "created the IPO"
 
-
+/nesi/project/niwa00004/rampaln/bin/python /nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/mjo_series/run_mjo.py
 
 git add *
 git commit -a -m "Auto updated"
