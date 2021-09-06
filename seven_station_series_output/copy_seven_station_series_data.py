05ab12961f7f14973d58ba31033163d4b34f08b8
Neelesh Rampal <neelesh.rampal@niwa.co.nz>
2021-09-06 12:05:51 +1200

    added updates and fixed seven station series code

--- seven_station_series_output/copy_seven_station_series_data.py
+++ seven_station_series_output/copy_seven_station_series_data.py
@@ -3,12 +3,15 @@ import shutil
 output_dirs = '/nesi/project/niwa00004/rampaln/CAOA2101/cpp-indices/seven_station_series_output'
 images = glob.glob(r'/nesi/project/niwa00004/rampaln/CAOA2101/seven-station-series-python/output/time_series/NZT7*',recursive = True)
 
-csv_files = glob.glob(r'/nesi/project/niwa00004/rampaln/CAOA2101/seven-station-series-python/output/time_series/*.csv')
+csv_files = glob.glob(r'/nesi/project/niwa00004/rampaln/CAOA2101/seven-station-series-python/output/time_series/Total_series_*.csv')
 
+csv_files1 = glob.glob(r'/nesi/project/niwa00004/rampaln/CAOA2101/seven-station-series-python/output/time_series/SevenStationSeries_full*.csv')
+csv_files2 = glob.glob(r'/nesi/project/niwa00004/rampaln/CAOA2101/seven-station-series-python/output/time_series/AllStationMonthly_Anomalies_*.csv')
+all_files = csv_files2 + csv_files + csv_files1
 for file in images:
     fname = file.split('/')[-1] 
     shutil.copy(file,f'{output_dirs}/figures/{fname}')
 
-for csv in csv_files:
+for csv in all_files:
     fname = csv.split('/')[-1]
     shutil.copy(csv,f'{output_dirs}/data/{fname}') 
